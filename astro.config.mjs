e878265c05a37a09abbd35b205c2c39ae9b78102
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-08-20 19:14:45 -0400

    adding favicon, SW, and nav component

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://astro-hackernews.pages.dev",
   output: "server",
   adapter: cloudflare()
 });
---
+++ public/sw.js
@@ -0,0 +1,1 @@
+self.addEventListener("fetch",e=>{(e.request.url.includes("localhost"))&&e.respondWith(caches.open("astro-hn").then(t=>t.match(e.request).then(n=>n||fetch(e.request).then(n=>(t.put(e.request,n.clone()),n)))))}),self.addEventListener("activate",e=>e.waitUntil(caches.delete("astro-hn")))
