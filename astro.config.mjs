c293620a33b8d5efe4240b99ce95b382875cb8e7
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-08-21 15:25:59 -0400

    migrating to netlify edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify/functions';
+import netlify from '@astrojs/netlify/edge';
 
 // https://astro.build/config
 export default defineConfig({
