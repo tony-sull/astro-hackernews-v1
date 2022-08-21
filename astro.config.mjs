9d92928dcb22c885e0d6223350d0d893d3456187
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-08-21 15:05:14 -0500

    migrating to deno deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+
+import deno from "@astrojs/deno";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-hackernews.pages.dev",
   output: "server",
-  adapter: vercel()
+  adapter: deno()
 });
